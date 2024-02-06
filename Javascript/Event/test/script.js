1b6e7648d297bf1247886e06e735dbb4ca2ddca4
Roberadesissai <roberadesissakit@gmail.com>
2024-02-05 19:48:23 -0500

    Disable axe/forms hint in .hintrc

---
+++ Javascript/Event/test/script.js
@@ -0,0 +1,19 @@
+function handleChange() {
+    alert('The selected value has changed!');
+}
+
+function handleClick() {
+    alert('Button was clicked!');
+}
+
+function handleMouseOver() {
+    document.getElementById('mouseoverExample').style.backgroundColor = 'lightblue';
+}
+
+function handleMouseOut() {
+    document.getElementById('mouseoverExample').style.backgroundColor = '#f0f0f0';
+}
+
+function handleKeyDown() {
+    alert('A key was pressed down!');
+}
